c8775be7317466e4f04c6e6daa89732c19662b16
Rodrigo <rodrigotorresmessenzani@hotmail.com>
2021-03-02 02:24:32 -0300

    counter app beta

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,35 @@
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+    const [counter, setCounter] = useState( value );
+
+    // handleAdd
+    const handleAdd = () => {
+        setCounter(counter + 1);
+    }
+    
+    const handleRest = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
     return (
         <div>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+            <button onClick={ handleAdd }>sumar +1</button> {/* No ejecutar la función, solo se lo llama sin el () xq onClick ya llama a la funcion */}
+            {/* habrá ocaciones que necesitemos darle un argumento y para eso usaremos una funcion flecha y llamamos a la funcion, pero normalmente es asi */}
+            <button onClick={ handleRest }>restar -1</button>
+            <button onClick={ handleReset }>reset</button>
         </div>
     )
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.string.isRequired // Convierte a numerico
+    value: PropTypes.number.isRequired // Convierte a numerico
 };
 
 export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<CounterApp value={100} />, divRoot);
+ReactDOM.render(<CounterApp value={0} />, divRoot);
 
 
