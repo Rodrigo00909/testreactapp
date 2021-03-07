ee9b18b5d30f2828ccb47e60b1831ef058f9802b
Rodrigo <rodrigotorresmessenzani@hotmail.com>
2021-03-07 15:48:31 -0300

    test 07

--- src/test/05-funciones.test.js
+++ src/test/05-funciones.test.js
@@ -1,20 +1,31 @@
-import { getUser } from "../base/05-funciones"
+import { getUser, getUsuarioActivo } from "../base/05-funciones"
 
 describe('Pruebas en 05-funciones.test.js', () => {
     test('getUser debe de retornar un objeto', () => {
         const userTest = {
             uid: 'ABC123',
-        username: 'El_Papi1502'
+            username: 'El_Papi1502'
         }
 
         const user = getUser();
         console.log(user);
 
-        expect(user).toStrictEqual(userTest)
-
-
+        expect(user).toStrictEqual(userTest) // Comparar dos objetos: toEqual
         
     })
+    // Getusuario activo debe retornar un objeto
+    test('getUsuarioActivo debe devolver un objeto', () => {
+        const nombre = 'Fabilo';
+
+        const user = getUsuarioActivo(nombre);
+        
+        expect(user).toStrictEqual({
+            uid: 'ABC567',
+            username: nombre
+        });
+        
+    })
+    
     
     
 })
